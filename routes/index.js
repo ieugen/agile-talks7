82292c4cf9f27e59c4bbfb7a6fe2165ed5d27fd1
Ioan Eugen Stan <ieugen@apache.org>
2015-06-10 17:45:51 +0300

    Welcome to agile talks 7

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Agile talks 7' });
 });
 
 module.exports = router;
